8a951db5fa0215e20e00a237eed34c43f49c10d2
PJ <goldprajnawi@gmail.com>
2021-06-14 17:22:33 +0800

    comment

--- src/PenAdapter.java
+++ src/PenAdapter.java
@@ -1,7 +1,7 @@
 public class PenAdapter implements Pen {
 
     PilotPen pp = new PilotPen();
-
+    // with this adapter , we can connect the pilot pen and pen
     @Override
     public void write(String s) {
         pp.mark(s);
