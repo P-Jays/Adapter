833b0b93c4dd8f4da89b6ecd4e0278ec352ce404
PJ <goldprajnawi@gmail.com>
2021-06-14 17:37:51 +0800

    comment 2

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // pen is an interface, so it cant declere new obj
         Pen p = new PenAdapter();
+        // we need an adapter to use pilot pen to write homwework
         WriteHomework wh = new WriteHomework();
         wh.setP(p);
         wh.writingHomework("I don't have homework anymore");
